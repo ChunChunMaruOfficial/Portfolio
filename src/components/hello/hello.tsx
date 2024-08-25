23d1b46a5e9f0b5e36937ed9584b82d2c30bccd3
ChunChunMaru <c4erkaoui@gmail.com>
2024-08-25 13:10:53 +0300

    228

--- src/components/hello/hello.tsx
+++ src/components/hello/hello.tsx
@@ -5,9 +5,9 @@ import style from './hello.module.scss'
 export default function Hello() {
     return (
         <div className={style.parent}>
-<p>Hi all. I am</p>
-<p>Micheal Weaver</p>
-<p> Front-end developer</p>
+            <p>Hi all. I am</p>
+            <p>Micheal Weaver</p>
+            <p> Front-end developer</p>
         </div>
     )
 }
